ec62c584af2ca92646e7c0e049eef8edeea6e6cc
rajashekardemo <123351969+rajashekardemo@users.noreply.github.com>
2023-11-21 17:47:16 +0530

    Update index.controller.ts

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -4,7 +4,7 @@ export const home = (req: Request, res: Response, next: NextFunction) => {
   try {
     return res.status(200).json({
       love: true,
-      message: 'You & Me',
+      message: 'You & Me🐱‍🐉✌😆🤔🤢😍💕👌😜',
     });
   } catch (error) {
     next(error);
